221d03345772ee923822e4a519b6ca90750b29e7
kotlarchik <kuplinov16@mail.ru>
2020-12-27 19:00:17 +0300

    update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,17 +1,10 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-
-import java.util.Optional;
 
 public class Main extends Application {
 
@@ -21,21 +14,6 @@ public class Main extends Application {
         primaryStage.setTitle("Курсы");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent windowEvent) {
-                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);//создаем окно подтверждения
-                //далее заголовки и текст по желанию
-                alert.setTitle("Выход");
-                alert.setHeaderText("Вы уверены? Все несохранённые данные будут утеряны.");
-                //создаем кнопки "да" и "нет"
-                ButtonType buttonTypeOne = new ButtonType("Да");
-                ButtonType buttonTypeCancel = new ButtonType("Нет", ButtonBar.ButtonData.CANCEL_CLOSE);
-                alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeCancel);//додаем кнопки к самому окну подтверждения
-                Optional<ButtonType> result = alert.showAndWait();//вызываем окно подтверждения
-                if(result.get() == buttonTypeCancel) windowEvent.consume(); //отменяем событие если пользователь нажал отмен
-            }
-        });
     }
 
     public static void main(String[] args) {
--- src/sample/controllers/Controller.java
+++ src/sample/controllers/Controller.java
@@ -4,8 +4,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
@@ -17,7 +17,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Controller {
-
     // Подключение к БД
     Connection connection = null;
     public Controller(){
@@ -28,55 +27,52 @@ public class Controller {
     ObservableList<Blank> javaList = FXCollections.observableArrayList();
     ObservableList<Blank> eclipseList = FXCollections.observableArrayList();
 
+    // Table OOP
     @FXML
-    private TableView tableOOP;
+    private TableView<Blank> tableOOP;
     @FXML
-    private TableColumn columnOOPstudent;
+    private TableColumn<Blank, String> columnOOPstudent;
     @FXML
-    private TableColumn columnOOPdate1;
+    private TableColumn<Blank, String> columnOOPdate1;
     @FXML
-    private TableColumn columnOOPdate2;
+    private TableColumn<Blank, String> columnOOPdate2;
     @FXML
-    private TableColumn columnOOPdate3;
+    private TableColumn<Blank, String> columnOOPdate3;
     @FXML
-    private TableColumn columnOOPdate4;
+    private TableColumn<Blank, String> columnOOPdate4;
 
+    // Table Java
     @FXML
-    private TableView tableJava;
+    private TableView<Blank> tableJava;
     @FXML
-    private TableColumn columnJavaStudent;
+    private TableColumn<Blank, String> columnJavaStudent;
     @FXML
-    private TableColumn columnJavadate1;
+    private TableColumn<Blank, String> columnJavadate1;
     @FXML
-    private TableColumn columnJavadate2;
+    private TableColumn<Blank, String> columnJavadate2;
     @FXML
-    private TableColumn columnJavadate3;
+    private TableColumn<Blank, String> columnJavadate3;
     @FXML
-    private TableColumn columnJavadate4;
+    private TableColumn<Blank, String> columnJavadate4;
 
+    // Table Eclipse
     @FXML
-    private TableView tableEclipse;
+    private TableView<Blank> tableEclipse;
     @FXML
-    private TableColumn columnEclipseStudent;
+    private TableColumn<Blank, String> columnEclipseStudent;
     @FXML
-    private TableColumn columnEclipsedate1;
+    private TableColumn<Blank, String> columnEclipsedate1;
     @FXML
-    private TableColumn columnEclipsedate2;
+    private TableColumn<Blank, String> columnEclipsedate2;
     @FXML
-    private TableColumn columnEclipsedate3;
+    private TableColumn<Blank, String> columnEclipsedate3;
     @FXML
-    private TableColumn columnEclipsedate4;
-
-    @FXML
-    private Button buttonSaveOOP;
-    @FXML
-    private Button buttonSaveJava;
-    @FXML
-    private Button buttonSaveEclipse;
+    private TableColumn<Blank, String> columnEclipsedate4;
 
     @FXML
     public void initialize(){
         try {
+            // OOP.
             String course = "OOP";
             String sqlSelectOOP = "SELECT * FROM blank WHERE courses = ?";
             PreparedStatement statement = connection.prepareStatement(sqlSelectOOP);
@@ -89,27 +85,59 @@ public class Controller {
                         resultSet.getString(4),
                         resultSet.getString(5),
                         resultSet.getString(6),
-                        resultSet.getString(7)));
+                        resultSet.getString(7)
+                ));
             }
             columnOOPstudent.setCellValueFactory(new PropertyValueFactory<>("student"));
 
             columnOOPdate1.setCellValueFactory(new PropertyValueFactory<>("date1"));
             columnOOPdate1.setCellFactory(TextFieldTableCell.<Blank>forTableColumn());
+            columnOOPdate1.setOnEditCommit((TableColumn.CellEditEvent<Blank, String> event) -> {
+                TablePosition<Blank, String> pos = event.getTablePosition();
+                String newRating = event.getNewValue();
+                int row = pos.getRow();
+                Blank blank = event.getTableView().getItems().get(row);
+                blank.setDate1(newRating);
+            });
 
             columnOOPdate2.setCellValueFactory(new PropertyValueFactory<>("date2"));
             columnOOPdate2.setCellFactory(TextFieldTableCell.<Blank>forTableColumn());
+            columnOOPdate2.setOnEditCommit((TableColumn.CellEditEvent<Blank, String> event) -> {
+                TablePosition<Blank, String> pos = event.getTablePosition();
+                String newRating = event.getNewValue();
+                int row = pos.getRow();
+                Blank blank = event.getTableView().getItems().get(row);
+                blank.setDate2(newRating);
+            });
 
             columnOOPdate3.setCellValueFactory(new PropertyValueFactory<>("date3"));
             columnOOPdate3.setCellFactory(TextFieldTableCell.<Blank>forTableColumn());
+            columnOOPdate3.setOnEditCommit((TableColumn.CellEditEvent<Blank, String> event) -> {
+                TablePosition<Blank, String> pos = event.getTablePosition();
+                String newRating = event.getNewValue();
+                int row = pos.getRow();
+                Blank blank = event.getTableView().getItems().get(row);
+                blank.setDate3(newRating);
+            });
 
             columnOOPdate4.setCellValueFactory(new PropertyValueFactory<>("date4"));
             columnOOPdate4.setCellFactory(TextFieldTableCell.<Blank>forTableColumn());
+            columnOOPdate4.setOnEditCommit((TableColumn.CellEditEvent<Blank, String> event) -> {
+                TablePosition<Blank, String> pos = event.getTablePosition();
+                String newRating = event.getNewValue();
+                int row = pos.getRow();
+                Blank blank = event.getTableView().getItems().get(row);
+                blank.setDate4(newRating);
+            });
 
             tableOOP.setItems(oopList);
-
+            tableOOP.setEditable(true);
+            tableOOP.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->
+                    showPersonDetails(newValue));
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+        // JAVA.
         try {
             String course = "Java";
             String sqlSelectJava = "SELECT * FROM blank WHERE courses = ?";
@@ -126,15 +154,57 @@ public class Controller {
                     resultSet.getString(7)));
             }
             columnJavaStudent.setCellValueFactory(new PropertyValueFactory<>("student"));
+
             columnJavadate1.setCellValueFactory(new PropertyValueFactory<>("date1"));
+            columnJavadate1.setCellFactory(TextFieldTableCell.<Blank>forTableColumn());
+            columnJavadate1.setOnEditCommit((TableColumn.CellEditEvent<Blank, String> event) -> {
+                TablePosition<Blank, String> pos = event.getTablePosition();
+                String newRating = event.getNewValue();
+                int row = pos.getRow();
+                Blank blank = event.getTableView().getItems().get(row);
+                blank.setDate1(newRating);
+            });
+
             columnJavadate2.setCellValueFactory(new PropertyValueFactory<>("date2"));
+            columnJavadate2.setCellFactory(TextFieldTableCell.<Blank>forTableColumn());
+            columnJavadate2.setOnEditCommit((TableColumn.CellEditEvent<Blank, String> event) -> {
+                TablePosition<Blank, String> pos = event.getTablePosition();
+                String newRating = event.getNewValue();
+                int row = pos.getRow();
+                Blank blank = event.getTableView().getItems().get(row);
+                blank.setDate2(newRating);
+            });
+
             columnJavadate3.setCellValueFactory(new PropertyValueFactory<>("date3"));
+            columnJavadate3.setCellFactory(TextFieldTableCell.<Blank>forTableColumn());
+            columnJavadate3.setOnEditCommit((TableColumn.CellEditEvent<Blank, String> event) -> {
+                TablePosition<Blank, String> pos = event.getTablePosition();
+                String newRating = event.getNewValue();
+                int row = pos.getRow();
+                Blank blank = event.getTableView().getItems().get(row);
+                blank.setDate3(newRating);
+            });
+
             columnJavadate4.setCellValueFactory(new PropertyValueFactory<>("date4"));
+            columnJavadate4.setCellFactory(TextFieldTableCell.<Blank>forTableColumn());
+            columnJavadate4.setOnEditCommit((TableColumn.CellEditEvent<Blank, String> event) -> {
+                TablePosition<Blank, String> pos = event.getTablePosition();
+                String newRating = event.getNewValue();
+                int row = pos.getRow();
+                Blank blank = event.getTableView().getItems().get(row);
+                blank.setDate4(newRating);
+            });
+
             tableJava.setItems(javaList);
+            tableJava.setEditable(true);
+            tableJava.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->
+                    showPersonDetails(newValue));
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
+
+        // ECLIPSE.
         try {
             String course = "Eclipse";
             String sqlSelectEclipse = "SELECT * FROM blank WHERE courses = ?";
@@ -151,24 +221,112 @@ public class Controller {
                     resultSet.getString(7)));
             }
             columnEclipseStudent.setCellValueFactory(new PropertyValueFactory<>("student"));
+
             columnEclipsedate1.setCellValueFactory(new PropertyValueFactory<>("date1"));
+            columnEclipsedate1.setCellFactory(TextFieldTableCell.<Blank>forTableColumn());
+            columnEclipsedate1.setOnEditCommit((TableColumn.CellEditEvent<Blank, String> event) -> {
+                TablePosition<Blank, String> pos = event.getTablePosition();
+                String newRating = event.getNewValue();
+                int row = pos.getRow();
+                Blank blank = event.getTableView().getItems().get(row);
+                blank.setDate1(newRating);
+            });
+
             columnEclipsedate2.setCellValueFactory(new PropertyValueFactory<>("date2"));
+            columnEclipsedate2.setCellFactory(TextFieldTableCell.<Blank>forTableColumn());
+            columnEclipsedate2.setOnEditCommit((TableColumn.CellEditEvent<Blank, String> event) -> {
+                TablePosition<Blank, String> pos = event.getTablePosition();
+                String newRating = event.getNewValue();
+                int row = pos.getRow();
+                Blank blank = event.getTableView().getItems().get(row);
+                blank.setDate2(newRating);
+            });
+
             columnEclipsedate3.setCellValueFactory(new PropertyValueFactory<>("date3"));
+            columnEclipsedate3.setCellFactory(TextFieldTableCell.<Blank>forTableColumn());
+            columnEclipsedate3.setOnEditCommit((TableColumn.CellEditEvent<Blank, String> event) -> {
+                TablePosition<Blank, String> pos = event.getTablePosition();
+                String newRating = event.getNewValue();
+                int row = pos.getRow();
+                Blank blank = event.getTableView().getItems().get(row);
+                blank.setDate3(newRating);
+            });
+
             columnEclipsedate4.setCellValueFactory(new PropertyValueFactory<>("date4"));
+            columnEclipsedate4.setCellFactory(TextFieldTableCell.<Blank>forTableColumn());
+            columnEclipsedate4.setOnEditCommit((TableColumn.CellEditEvent<Blank, String> event) -> {
+                TablePosition<Blank, String> pos = event.getTablePosition();
+                String newRating = event.getNewValue();
+                int row = pos.getRow();
+                Blank blank = event.getTableView().getItems().get(row);
+                blank.setDate4(newRating);
+            });
+
             tableEclipse.setItems(eclipseList);
+            tableEclipse.setEditable(true);
+            tableEclipse.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->
+                    showPersonDetails(newValue));
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
 
+    private void showPersonDetails(Blank blank) {
+        if (blank != null){
+            System.out.println(blank.getId() + " " + blank.getStudent() + " " + blank.getCourses() + " " + blank.getDate1() + " " +
+                    blank.getDate2() + " " + blank.getDate3() + " " + blank.getDate4());
+        }
+    }
+
+    // Button save OOP.
     public void pressSaveOOP(ActionEvent event){
-
+        try {
+            Blank blankOOP = tableOOP.getSelectionModel().getSelectedItem();
+            String sqlUpdateOOP = "UPDATE blank SET dateOne = ?, dateTwo = ?, dateThree = ?, dateFour = ? WHERE id=?";
+            PreparedStatement statement = connection.prepareStatement(sqlUpdateOOP);
+            statement.setString(1, blankOOP.getDate1());
+            statement.setString(2, blankOOP.getDate2());
+            statement.setString(3, blankOOP.getDate3());
+            statement.setString(4, blankOOP.getDate4());
+            statement.setInt(5, blankOOP.getId());
+            statement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
+
+    // Button save Java.
     public void pressSaveJava(ActionEvent event){
-
+        try {
+            Blank blankJava = (Blank) tableJava.getSelectionModel().getSelectedItem();
+            String sqlUpdateJava = "UPDATE blank SET dateOne = ?, dateTwo = ?, dateThree = ?, dateFour = ? WHERE id=?";
+            PreparedStatement statement = connection.prepareStatement(sqlUpdateJava);
+            statement.setString(1, blankJava.getDate1());
+            statement.setString(2, blankJava.getDate2());
+            statement.setString(3, blankJava.getDate3());
+            statement.setString(4, blankJava.getDate4());
+            statement.setInt(5, blankJava.getId());
+            statement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
-    public void pressSaveEclipse(ActionEvent event){
 
+    // Button save Eclipse.
+    public void pressSaveEclipse(ActionEvent event){
+        try {
+            Blank blankEclipse = (Blank) tableEclipse.getSelectionModel().getSelectedItem();
+            String sqlUpdateEclipse = "UPDATE blank SET dateOne = ?, dateTwo = ?, dateThree = ?, dateFour = ? WHERE id=?";
+            PreparedStatement statement = connection.prepareStatement(sqlUpdateEclipse);
+            statement.setString(1, blankEclipse.getDate1());
+            statement.setString(2, blankEclipse.getDate2());
+            statement.setString(3, blankEclipse.getDate3());
+            statement.setString(4, blankEclipse.getDate4());
+            statement.setInt(5, blankEclipse.getId());
+            statement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
